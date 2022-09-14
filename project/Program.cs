305262f00653a09f88f3c6b0c054e44358ef8289
Heloise Dafforne <17DaffoH@fulford.york.sch.uk>
2022-09-14 12:40:17 +0100

    added code

--- project/Program.cs
+++ project/Program.cs
@@ -10,21 +10,58 @@ namespace project
     {
         static void Main(string[] args)
         {
-            Console.Write("What flavour do you want? ");
-            string pizza = Console.ReadLine();
+            string pizza = Input("what pizza would you like? ");
+            
 
             if (pizza == "ham" || pizza == "cheese")
             {
-                Console.WriteLine($"okay, one {pizza} pizza!");
+                Print($"okay, one {pizza} pizza!", ConsoleColor.DarkYellow);
             }
 
             switch(pizza)
             {
                 case "cheese":
                 case "margherita":
-                    Console.WriteLine("Suitable for vegetarians");
-                
+                    Print("Suitable for vegetarians", ConsoleColor.Green);
+                    break;
             }
+
+            string music = Input("what music do you like? ");
+
+            switch (music)
+            {
+                case "rock":
+                case "metal":
+                case "jazz":
+                    Print("okay", ConsoleColor.Blue);
+                    break;
+                case "pop":
+                    Print("okay", ConsoleColor.DarkMagenta);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// prints a line
+        /// </summary>
+        /// <param name="line">the line to be printed</param>
+        private static void Print(string line, ConsoleColor colour)
+        {
+            Console.ForegroundColor = colour;
+            Console.WriteLine(line);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+
+        /// <summary>
+        /// ask user a question
+        /// </summary>
+        /// <param name="question">the question asked</param>
+        /// <returns>the user answer</returns>
+        private static string Input(string question)
+        {
+            Console.Write(question);
+            return  Console.ReadLine();
         }
     }
 }
