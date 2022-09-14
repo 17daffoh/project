2c4ca053314a6680b206eb13f293e515a107cd03
Heloise Dafforne <17DaffoH@fulford.york.sch.uk>
2022-09-14 12:04:33 +0100

    Add project files.

---
+++ project/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What flavour do you want? ");
+            string pizza = Console.ReadLine();
+
+            if (pizza == "ham" || pizza == "cheese")
+            {
+                Console.WriteLine($"okay, one {pizza} pizza!");
+            }
+
+            switch(pizza)
+            {
+                case "cheese":
+                case "margherita":
+                    Console.WriteLine("Suitable for vegetarians");
+                
+            }
+        }
+    }
+}
